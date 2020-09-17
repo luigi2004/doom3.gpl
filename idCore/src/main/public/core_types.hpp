133a6a3f99a3dfdaa5a67257f9788bc2bad5d7d7
Orlando Wiley <owiley2002@gmail.com>
2020-09-16 21:52:32 -0400

    adding core type intro

---
+++ idCore/src/main/public/core_types.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+using sint = signed int;
+using uint = unsigned int;
+
+using slong = signed long;
+using ulong = unsigned long;
+
+using byte = unsigned char;
+using sbyte = signed char;
+
